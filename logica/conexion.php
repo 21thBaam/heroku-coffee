5f8ef60ac0d9924ecc8ddc0ba7365102497d7e62
Angel Reyes <51466636+21thBaam@users.noreply.github.com>
2020-06-11 21:23:59 -0500

    Update conexion.php

--- logica/conexion.php
+++ logica/conexion.php
@@ -1,8 +1,8 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "camposgranos";
+    $servername = "qf5dic2wzyjf1x5x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $username = "kw1fn8ikyc3t8a02";
+    $password = "fbc7ihss5i02y7oo";
+    $dbname = "ypg9uq715ye7be79";
     
     $conn = new mysqli($servername, $username, $password, $dbname);
     
